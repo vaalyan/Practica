c238bb15f2029aecf0aeddf7936297de5ad5abaa
vaalyan <vaalyan32@gmail.com>
2024-08-09 22:33:49 -0500

    practica js

---
+++ js-foundation/6Mayor10.js
@@ -0,0 +1,12 @@
+var numero1 = prompt("Escriba un número aleatorio.");
+/* var numero2 = 10;
+
+console.log(numero1===numero2 || numero1>numero2 || numero1<numero2
+    (`El numero es mayor a 10.`)
+); */
+
+if(numero1 > 10) {
+    console.log('El número es mayor a 10');
+} else {
+    console.log('El número es menor o igual a 10')
+}
